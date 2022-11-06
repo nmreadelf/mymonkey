82f32086bc510b4c110214392885e310c859c99a
nmreadelf <frank.q.kong@hotmail.com>
2022-11-06 13:02:51 +0800

    Feat let and return statment add value for them

--- parser/parser.go
+++ parser/parser.go
@@ -117,9 +117,10 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+	p.nextToken()
 
-	// TODO: We're skipping the expressinos until we
-	// encounter a semicolon
+	stmt.Value = p.parseExpression(LOWEST)
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
@@ -132,8 +133,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// TODO: We're skipping the expressinos until we
-	// encounter a semicolon
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
