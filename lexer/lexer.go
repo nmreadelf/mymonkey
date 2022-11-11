4def53e5f1ae2b9ccea254890fe8888ee4b5d770
nmreadelf <frank.q.kong@hotmail.com>
2022-11-11 10:32:47 +0800

    Impl string lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -62,6 +62,18 @@ func (l *Lexer) readNumber() string {
 	return l.input[pos:l.position]
 }
 
+func (l *Lexer) readString() string {
+	position := l.position + 1
+	for {
+		l.readChar()
+		if l.ch == '"' || l.ch == 0 {
+			break
+		}
+	}
+
+	return l.input[position:l.position]
+}
+
 func (l *Lexer) peekChar() byte {
 	if l.readPosition >= len(l.input) {
 		return 0
@@ -113,6 +125,9 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '"':
+		tok.Type = token.STRING
+		tok.Literal = l.readString()
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
